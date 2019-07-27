f4c117d144c4c29e4f171ff75520c9726c63309e
ketchom78 <nkeneng.junior@gmail.com>
2019-07-27 12:01:26 +0200

    nothing important

--- frontend/show-category.php
+++ frontend/show-category.php
@@ -12,33 +12,22 @@ $result = mysqli_query($db, $query);
             $array[] = $row;
             $image = $row['junior2_pic'];
             ?>
-            <div class="col"><a href=""><img src='../Assets/upload/<?php echo $image; ?>' class="pho"> </a>
+            <div class="col">
+                <img src='../Assets/upload/<?php echo $image; ?>'>
                 <div class="col-body">
-                    <h4 class="col-title">
-                        <div class="col-head"><a href=""><?php echo $row['junior2_nom'] ?></a>
-                            <?php echo $row['junior2_prix'] ?> €
+                    <h4>
+                        <div class="col-head">
+                            <h2> <?php echo $row['junior2_nom'] ?></h2>
+                            <p> <?php echo $row['junior2_prix'] ?> €</p>
                         </div>
                     </h4>
-                    <p style="text-align: center">
-                        <?php
-                        echo $row['junior2_cat'];
-                        ?>
-                    </p>
-                    <!---Redirection ver la page  de modifier objet----------->
-                    <?php echo "<a href='modifier_objet.php?id=" . $row['junior2_id'] . "'><button class='su' type='submit' > Bearbeiten</button></a>"; ?>
-
-                    <!--------Redirection ver la page reserver article ----------------->
-                    <!--                    --><?php //echo "<a href='reserver.php?id=" . $row['id'] . "'><button class='su' type='submit' >Réserver</button></a>"; ?>
-
-                    <!---Redirection ver la page  de supprimer objet----------->
-                    <!--                    --><?php //echo "<a href='supprimer_objet.php?id=".$row['id']."' onclick='produktentfernen()'><button class='sub' type='submit' > Supprimer</button></a>";?>
-                    <button class="sub" id="delete" onclick="produktentfernen(<?php echo $row['junior2_id']; ?>)"
-                            type="button">
-                        Löschen
-                    </button>
-
+                    <p><?php echo $row['junior2_cat']; ?> </p>
+                    <div class="btn">
+                        <a class="button" href="modifier_objet.php?id=<?php echo $row['junior2_id'] ?>">modify</a>
+                        <button type="button" onclick="produktentfernen(<?php echo $row['junior2_id']; ?>)">delete
+                        </button>
+                    </div>
                 </div>
-
             </div>
         <?php } ?>
         <br>
