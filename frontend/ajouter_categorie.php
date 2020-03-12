2cbaa3a822ed159be1fd74a00409af890f5cbcf0
nkeneng <nkeneng.junior@gmail.com>
2020-03-12 17:33:22 +0100

    test for source tree

--- frontend/ajouter_categorie.php
+++ frontend/ajouter_categorie.php
@@ -2,6 +2,8 @@
 include "layout.php";
 ?>
 <br>
+
+
 <form action="../backend/ajouter_categoriedb.php" method="post" enctype='multipart/form-data'>
     <h2>Add category</h2><br/>Name<br/>
     <input type="text" name="cat" class="form" placeholder="Insert the category" required>
